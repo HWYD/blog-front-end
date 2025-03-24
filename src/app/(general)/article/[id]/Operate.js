4d334b23c12aa1babf77ed046487cda47eae1174
weiyue.huang <weiyue.huang@adspower.net>
2025-03-24 14:03:43 +0800

    新增删除文章

---
+++ src/app/(general)/article/[id]/Operate.js
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+import { Button, message, Popconfirm } from 'antd';
+import { fetchData } from '@/api';
+import { useRouter } from 'next/navigation'
+
+
+const Operate = ({articleId}) => {
+    const router = useRouter()
+    const confirm = async() => {
+      console.log('articleId',articleId);
+        const params = {
+            id: articleId
+        };
+        const queryParams = new URLSearchParams(params);
+        await fetchData(`/article?${queryParams.toString()}`,{
+            method: 'DELETE',
+        })
+      message.success('删除成功');
+      setTimeout(()=>{
+        window.open('/','_self')
+      },2000)
+    };
+  return (<Popconfirm
+            title="删除提示"
+            description="确定删除该篇文章吗?"
+            onConfirm={confirm}
+            okText="确定"
+            cancelText="取消"
+        >
+            <Button size="small" danger type="text" className='ml-4'>删除</Button>
+        </Popconfirm>)
+}
+export default Operate;
--- src/app/(general)/article/[id]/page.js
+++ src/app/(general)/article/[id]/page.js
@@ -1,13 +1,13 @@
 import { fetchData } from '@/api';
 import ReactMarkdown from'react-markdown';
-import { Tag } from 'antd';
+import { Tag, Button } from 'antd';
 import { cookies } from 'next/headers'
 import { EyeOutlined } from '@ant-design/icons';
 import { convertDate } from '@/utils'
 import Link from 'next/link'
+import Operate from './Operate'
 
 export default async function article(context) {
-
     const cookieStore = cookies()
     const authorization = cookieStore.get('authorization')?.value || ''
     const articleId = context.params.id
@@ -27,18 +27,24 @@ export default async function article(context) {
             <span className='text-gray-800'>{ articleData.author }</span>
             <span className='ml-4'>{ convertDate(articleData.create_time) }</span>
             <span className='ml-4'><EyeOutlined className='mr-1'/>{ articleData.view_num }</span>
-            { articleData.is_author == '1'?  
-            <Link href= {`/drafts?id=${articleId}`}><span className='ml-4 text-blue-500 cursor-pointer no-underline hover:text-blue-600'>编辑</span></Link> :''}
+            { articleData.is_author == '1' || articleData.is_admin == '1'?  
+              <Link href= {`/drafts?id=${articleId}`}><span className='ml-4 text-blue-500 cursor-pointer no-underline hover:text-blue-600'>编辑</span></Link> :''
+            }
+            { articleData.is_admin == '1'?  
+              <Operate articleId={articleId} ></Operate> :''
+            }
           </div>
           <div>
               <ReactMarkdown>
                   {articleData.content}
               </ReactMarkdown>
-              <div className='mt-10'>
-                <span className='mr-3 text-sm'>标签：</span>{ articleData.Tags.map(tagItem => (
-                    <Tag key={tagItem.id}>{tagItem.name}</Tag>
-                  )) }
-              </div>
+              {
+                articleData.Tags.length? <div className='mt-10'>
+                  <span className='mr-3 text-sm'>标签：</span>{ articleData.Tags.map(tagItem => (
+                      <Tag key={tagItem.id}>{tagItem.name}</Tag>
+                    )) }
+                </div> : ''
+              }
           </div>
         </div>
       </div>
