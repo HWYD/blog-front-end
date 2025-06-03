328bcc78a2b32f258c39fad125c3b929baff0bb3
weiyue.huang <weiyue.huang@adspower.net>
2025-06-03 15:28:53 +0800

    保存自动格式化

--- src/app/component/Header.js
+++ src/app/component/Header.js
@@ -1,22 +1,21 @@
-import { Button } from 'antd';
-import Link from 'next/link'
 import { cookies } from 'next/headers'
+import Link from 'next/link'
 import UserProfile from './UserProfile'
 
 export default function Header() {
-    const cookieStore = cookies()
-    const authorization = cookieStore.get('authorization')?.value || ''
-    const loginStatus = !!authorization
-    return (
-        <>
-            <header className="py-2 bg-white">
-                <div className="flex justify-between items-center mx-auto w-full 2xl:max-w-[1280px] px-2 box-border">
-                    <Link href="/"><img src='/image/logo.png' className="w-12"></img></Link>
-                    <div>
-                        <UserProfile loginStatus={loginStatus} />
-                    </div>
-                </div>
-            </header>
-        </>
-    )
+  const cookieStore = cookies()
+  const authorization = cookieStore.get('authorization')?.value || ''
+  const loginStatus = !!authorization
+  return (
+    <>
+      <header className="py-2 bg-white">
+        <div className="flex justify-between items-center mx-auto w-full 2xl:max-w-[1280px] px-2 box-border">
+          <Link href="/"><img src="/image/logo.png" className="w-12"></img></Link>
+          <div>
+            <UserProfile loginStatus={loginStatus} />
+          </div>
+        </div>
+      </header>
+    </>
+  )
 }
